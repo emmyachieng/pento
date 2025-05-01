6827877226c5558dff0a94a281f75fa18b3d5bf3
emmy achieng <emmy@investbamboo.com>
2025-05-02 02:14:27 +0300

    added auth generator

---
+++ lib/pento_web/live/wrong_live.ex
@@ -0,0 +1,43 @@
+defmodule PentoWeb.WrongLive do
+  use PentoWeb, :live_view
+
+  def mount(_params, session, socket) do
+    {:ok, assign(socket, score: 0, message: "Guess a number.", session_id: session["live_socket_id"])}
+  end
+
+  @spec render(any) :: Phoenix.LiveView.Rendered.t()
+  def render(assigns) do
+    ~H"""
+      <h1 class="mb-4 text-4xl font-extrabold">Your score: <%= @score %></h1>
+      <h2>
+      <%= @message %>
+      </h2>
+      <br/>
+      <h2>
+      <%= for n <- 1..10 do %>
+      <.link class="bg-blue-500 hover:bg-blue-700
+      text-white font-bold py-2 px-4 border border-blue-700 rounded m-1"
+      phx-click="guess" phx-value-number= {n} >
+      <%= n %>
+      </.link>
+      <% end %>
+      </h2>
+      <br/>
+      <pre>
+      <%= @current_user.email %>
+      <%= @session_id %>
+
+      </pre>
+    """
+  end
+
+  def time() do
+    DateTime.utc_now |> to_string
+  end
+
+  def handle_event("guess", %{"number" => guess}, socket) do
+    message = "Your guess: #{guess}. Wrong. Guess again. "
+    score = socket.assigns.score - 1
+    {:noreply, assign(socket, message: message, score: score)}
+  end
+end
